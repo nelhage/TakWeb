561eb52876312346b5701ce99e0b7aeabc82c9e6
TreffnonX <janis@schoeck-netz.de>
2016-09-11 00:58:22 +0200

    Update chat.js

--- js/logic/serverops/chat.js
+++ js/logic/serverops/chat.js
@@ -77,8 +77,8 @@ function printChatMessage (types, user, message)
   message = message.linkify(options);
 
   // highlight own name.
-  message = message.replace(new RegExp('(?:^|\\s)' + server.myname + '(?:$|\\s)', 'gi'),
-      '<span class="chatmyname">$&</span>');
+  message = message.replace(new RegExp('(^|[^\\w\\d])(' + server.myname + ')($|[\\W\\d])', 'gi'),
+      '$1<span class="chatmyname">$2</span>$3');
   chatMessage.append(message + '<br>');
 
   // append message to chat.
